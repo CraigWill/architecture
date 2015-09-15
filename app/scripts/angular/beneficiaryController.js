85c2d32f954bc67f90e18122e06f165542d27010
CraigWill <jingwei_xu@hotmail.com>
2015-09-15 09:44:48 +0800

    Start Version

---
+++ app/scripts/angular/beneficiaryController.js
@@ -0,0 +1,38 @@
+'use strict';
+
+app
+    .controller('deneficiaryCtrl', function($scope, dateFilter) {
+        $scope.isChecked = false;
+        $scope.add = function() {
+            $scope.row.beneficiary.proportion = 
+                (parseInt($scope.row.beneficiary.proportion) +
+                    parseInt($scope.rest));
+        };
+
+        $scope.moveDesc = function(isDesc, index) {
+            $scope.$emit('move-row-desc', {
+                isDesc: isDesc,
+                index: index
+            });
+        };
+
+        $scope.$on('check-all-items', function(event, data) {
+            $scope.isChecked = true;
+        });
+
+        $scope.checkIdentification = function(cardNo, birthdate) {
+            var givenYear = birthdate.getFullYear();
+            var givenMonth =birthdate.getMonth()+1;
+            var givenDay = birthdate.getDate();
+            var year = cardNo.substring(6, 10);
+            var month = cardNo.substring(10, 12);
+            var day = cardNo.substring(12, 14);
+            if(Number(givenYear) !== Number(year) || Number(givenMonth)!== Number(month) || Number(givenDay)!== Number(day)) {
+                return false;
+            }else {
+                return true;
+            }
+
+        };
+
+    });
